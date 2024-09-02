8d47952336cb4fe9a367c9a45597dcca693bbb8f
Ryne Flood <ryne.flood@getvish.com>
2024-09-02 10:59:12 -0400

    debug

--- lua/neotest-scala/framework.lua
+++ lua/neotest-scala/framework.lua
@@ -62,7 +62,7 @@ local function build_command_with_test_path(project, runner, test_path, extra_ar
         "sbt",
         -- "--no-colors",
         extra_args,
-        "' .. subcommand_joined .. '",
+        '"' .. subcommand_joined .. '"',
     })
 
     local joined = table.concat(ret, " ")
